63737686900750125084bd5d9b4567b7c1366075
Juan Soto <jpsoto121@gmail.com>
2023-03-07 23:35:55 -0500

    some modifications to settings.py

--- core/settings.py
+++ core/settings.py
@@ -21,12 +21,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "127.0.0.1"
+    "127.0.0.1",
+    "44.201.29.87"
 ]
 
 if not DEBUG:
     ALLOWED_HOSTS = [
         'https://juansoto10.github.io',
+        '44.201.29.87'
     ]
     
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -123,21 +125,25 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:5500',
     'http://localhost:8000',
+    '44.201.29.87'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5500',
     'http://localhost:8000',
+    '44.201.29.87'
 ]
 
 if not DEBUG:
     CORS_ORIGIN_WHITELIST = [
         'https://juansoto10.github.io',
+        '44.201.29.87',
     ]
     
     CSRF_TRUSTED_ORIGINS = [
         'https://juansoto10.github.io',
+        '44.201.29.87',
     ]
 
 
@@ -219,37 +225,37 @@ FILE_UPLOAD_PERMISSIONS = 0o640
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 
 
-if not DEBUG:
-    DEFAULT_FROM_EMAIL="Juan <sjuan23p@gmail.com>"
-    EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+# if not DEBUG:
+#     DEFAULT_FROM_EMAIL="Juan <sjuan23p@gmail.com>"
+#     EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+#     EMAIL_HOST = env('EMAIL_HOST')
+#     EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+#     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+#     EMAIL_PORT = env('EMAIL_PORT')
+#     EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 
     
-    # django-ckeditor will not work with S3 through django-storages without this line in settings.py
-    AWS_QUERYSTRING_AUTH = False
+#     # django-ckeditor will not work with S3 through django-storages without this line in settings.py
+#     AWS_QUERYSTRING_AUTH = False
 
-    # aws settings
-    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+#     # aws settings
+#     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+#     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+#     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
 
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    AWS_DEFAULT_ACL = 'public-read'
+#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+#     AWS_DEFAULT_ACL = 'public-read'
 
-    # s3 static settings
+#     # s3 static settings
 
-    STATIC_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#     STATIC_LOCATION = 'static'
+#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    # s3 public media settings
+#     # s3 public media settings
 
-    PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStore'
+#     PUBLIC_MEDIA_LOCATION = 'media'
+#     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+#     DEFAULT_FILE_STORAGE = 'core.storage_backends.MediaStore'
